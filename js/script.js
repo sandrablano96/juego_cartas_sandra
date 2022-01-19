e986116a06e4ebff86a699aab941619dadec21de
sandrablano96 <sandrablano96@gmail.com>
2022-01-19 20:04:56 +0100

    ultimos cambios

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var contadorClicks = 0;
-
-
-
+var cont = 0;
 let imagenesCarta = document.querySelectorAll(".carta__imagen");
 let cartas = document.querySelectorAll(".carta");
 
@@ -15,25 +13,33 @@ for (carta of cartas) {
         let contenidoCarta = evt.target;
         let idCarta = contenidoCarta.parentElement.id;
         contadorClicks++;
-
+        let cartasSeleccionadas = new Array();
         for (let i = 0; i <= 12; i++) {
             if (idCarta == i) {
-                contenidoCarta.lastElementChild.style.display = "block";
-                contenidoCarta.firstElementChild.style.display = "none";
+                this.lastElementChild.style.display = "block";
+                this.firstElementChild.style.display = "none";
                 this.removeEventListener("click", mostrarImagenes, false);
-               
-            } 
-            let cartasSeleccionadas = array();
-            
-            if (contadorClicks == 2){
 
-                comprobarIguales(  )
+                cartasSeleccionadas.push(contenidoCarta.lastElementChild.className.split(" ")[1]);
+            }
+            if (contadorClicks == 2) {
+                comprobarIguales(cartasSeleccionadas);
+                break;
             }
         }
-        
+
     });
 }
 
-function comprobarIguales(sel1,sel2){
+function comprobarIguales(arraySeleccionados) {
+    console.log(arraySeleccionados);
+    if(arraySeleccionados[0] == arraySeleccionados[1]){
+        console.log("verdadero");
+        cont++;
+        /*function checkTotal(){
 
+        }*/
+    }else{
+        console.log("falso");
+    }
 }
