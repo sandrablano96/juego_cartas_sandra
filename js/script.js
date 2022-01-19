9e514c7f57380ae4505f5d45a36fbf0e812284f5
sandrablano96 <sandrablano96@gmail.com>
2022-01-19 19:48:14 +0100

    cambios 19/01

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-let imagenCarta = document.querySelectorAll(".carta > div");
-let carta = document.querySelectorAll(".carta");
+var contadorClicks = 0;
 
-for (let i = 0; i < carta.length; i++) {
-    imagenCarta[i].style.display = "none";
-    carta[i].setAttribute("id", i);
+
+
+let imagenesCarta = document.querySelectorAll(".carta__imagen");
+let cartas = document.querySelectorAll(".carta");
+
+for (let i = 0; i < cartas.length; i++) {
+    imagenesCarta[i].style.display = "none";
+    cartas[i].setAttribute("id", i);
 }
 
-for (contenidoCarta of carta) {
-    contenidoCarta.addEventListener("click", function(evt){
+for (carta of cartas) {
+    carta.addEventListener("click", function mostrarImagenes(evt) {
         let contenidoCarta = evt.target;
-        let idCarta = contenidoCarta.parentElement.id
-        
-            for (let i = 0; i <= 12; i++) {
-                if(idCarta == i){
-                    imagenCarta[i].style.display = "block";
-                    break;
-                }
-            }
+        let idCarta = contenidoCarta.parentElement.id;
+        contadorClicks++;
+
+        for (let i = 0; i <= 12; i++) {
+            if (idCarta == i) {
+                contenidoCarta.lastElementChild.style.display = "block";
+                contenidoCarta.firstElementChild.style.display = "none";
+                this.removeEventListener("click", mostrarImagenes, false);
+               
+            } 
+            let cartasSeleccionadas = array();
             
+            if (contadorClicks == 2){
+
+                comprobarIguales(  )
+            }
+        }
+        
     });
+}
+
+function comprobarIguales(sel1,sel2){
+
 }
