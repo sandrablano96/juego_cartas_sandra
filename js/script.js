11698a493b856a8295c445383687c5c1c2dfbd75
sandrablano96 <sandrablano96@gmail.com>
2022-01-20 17:55:36 +0100

    version sin contador

--- js/script.js
+++ js/script.js
@@ -1,46 +1,105 @@
 var contadorClicks = 0;
 var cont = 0;
 var cartasSeleccionadas = new Array();
-let imagenesCarta = document.querySelectorAll(".carta__imagen");
 let cartas = document.querySelectorAll(".carta");
+var imagen
+//creamos dos arrays aleatorios del 1 al 6
+let opciones1 = [1, 2, 3, 4, 5, 6];
+let opciones2 = [1, 2, 3, 4, 5, 6];
+shuffle(opciones1);
+shuffle(opciones2);
+let arrayOpciones = opciones1.concat(opciones2);
 
 for (let i = 0; i < cartas.length; i++) {
-    imagenesCarta[i].style.display = "none";
-    cartas[i].setAttribute("id", i);
+    cartas[i].setAttribute("name", arrayOpciones[i]);
 }
+//funcion que mezcla los numeros e impide repeticiones, cambiar por sort + random
+function shuffle(array) {
+    let counter = array.length;
 
-for (carta of cartas) {
-    carta.addEventListener("click", function mostrarImagenes(evt) {
-        let contenidoCarta = evt.target;
-        let idCarta = contenidoCarta.parentElement.id;
-        contadorClicks++;
-        
-        for (let i = 0; i <= 12; i++) {
-            if (idCarta == i) {
-                this.lastElementChild.style.display = "block";
-                this.firstElementChild.style.display = "none";
-                this.removeEventListener("click", mostrarImagenes, false);
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
 
-                cartasSeleccionadas.push(contenidoCarta.lastElementChild.className.split(" ")[1]);
-            }
-            if (contadorClicks == 2) {
-                comprobarIguales(cartasSeleccionadas);
-                break;
-            }
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+function mostrarImagenes(evt) {
+    contadorClicks++;
+    let carta = evt.target;
+    console.log(carta);
+    if (carta != null) {
+        /*let carta = contenidoCarta.parentElement;*/
+        let idCarta = carta.getAttribute("name");
+        cartasSeleccionadas.push(carta);
+        imagen = document.createElement("img");
+        //corregir, se agrega debajo del div de carta__imagen
+        carta.appendChild(imagen).setAttribute("src", "img/" + idCarta + ".png");
+        carta.style.backgroundColor = "white";
+        carta.style.backgroundImage = "url('')";
+        carta.style.border = "1px solid black";
+        this.firstElementChild.style.display = "none";
+        if (contadorClicks == 2) {
+            contadorClicks = 0;
+            setTimeout(() => {
+                deseleccionar();
+              }, 1000);
+            
         }
+    }
 
-    });
 }
+//añade a cada div carta el evento
+for (carta of cartas) {
+    carta.addEventListener("click", mostrarImagenes);
+}
+
+
+function deseleccionar() {
+    if (comprobarIguales(cartasSeleccionadas)) {
+        cartasSeleccionadas.forEach(element => {
+            element.removeEventListener("click", mostrarImagenes);
+        });
+
+    } else{
+            cartasSeleccionadas.forEach(element => {
+                element.removeChild(element.lastElementChild);
+                element.addEventListener("click", mostrarImagenes);
+                //prodiamos crear una clase con dos estilos (inicial, acertada) y quitar o darla a los elementos
+                element.style.backgroundImage = "url(img/cara-trasera.jpg)";
+                element.style.backgroundColor = "";
+                element.style.border= "";
+
+            });
+
+    };
+    cartasSeleccionadas = [];
+
+}
+
 
 function comprobarIguales(arraySeleccionados) {
-    console.log(arraySeleccionados);
-    if(arraySeleccionados[0] == arraySeleccionados[1]){
+    if (arraySeleccionados[0].getAttribute("name") == arraySeleccionados[1].getAttribute("name")) {
+        arraySeleccionados.forEach(element => {
+            element.style.border = "3px solid green"
+        });
         console.log("verdadero");
         cont++;
+        return true;
         /*function checkTotal(){
 
         }*/
-    }else{
+    } else {
         console.log("falso");
+        return false
     }
 }
