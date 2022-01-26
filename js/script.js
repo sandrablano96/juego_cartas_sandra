95def0694324ca937a452f0276d6726025d4ae6d
sandrablano96 <sandrablano96@gmail.com>
2022-01-26 19:24:00 +0100

    Corregido a 26/01

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
 var contadorClicks = 0;
-var cont = 0;
+var cont = document.getElementById("contador");
+var errores = document.getElementById("errores");
 var cartasSeleccionadas = new Array();
 let cartas = document.querySelectorAll(".carta");
-var imagen
-//creamos dos arrays aleatorios del 1 al 6
+var imagen;
+
+//creamos el array de cartas
 let opciones = ["bart", "lisa", "bart", "maggie", "maggie", "homer", "flanders", "lisa", "flanders", "marge", "marge", "homer"];
 let arrayOpciones = opciones.sort(function () { return Math.random() - 0.5 });
+
+//ponemos el idioma, si ya hay almacenado uno, sino por defecto español
+if (localStorage.getItem("idioma") != null) {
+    cambiaTexto(localStorage.getItem("idioma"));
+} else {
+    cambiaTexto("esp");
+}
 
 for (let i = 0; i < arrayOpciones.length; i++) {
     cartas[i].setAttribute("name", arrayOpciones[i]);
 }
-//funcion que mezcla los numeros e impide repeticiones, cambiar por sort + random
-function shuffle(array) {
-    let counter = array.length;
+//DATOS USUARIO
+do {
+    usuario = prompt("Introduce tu nombre de usuario",)
+} while (usuario === "" || usuario == null)
+let etiquetaUsuario = document.getElementById("usuario").innerHTML = usuario;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+//Top player
+let filaError = document.getElementById("jugadorTopFilaError")
+let jugadorTop = document.getElementById("jugadorTop")
 
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
+if (localStorage.getItem("errores") != null && localStorage.getItem("usuario") != null) {
+    filaError.innerHTML = localStorage.getItem("errores")
+    jugadorTop.innerHTML = localStorage.getItem("usuario")
 }
+
+
+//añade a cada div carta el evento
+for (carta of cartas) {
+    carta.addEventListener("click", mostrarImagenes);
+}
+
 function mostrarImagenes(evt) {
     contadorClicks++;
     let carta = evt.target;
@@ -45,30 +55,27 @@ function mostrarImagenes(evt) {
         carta.style.backgroundColor = "white";
         carta.style.backgroundImage = "url('')";
         carta.style.border = "1px solid black";
+        carta.removeEventListener("click", mostrarImagenes);
         if (contadorClicks == 2) {
             contadorClicks = 0;
             setTimeout(() => {
-                deseleccionar();
-            }, 1000);
+                deseleccionar(cartasSeleccionadas);
+            }, 500);
+
 
         }
     }
 
 }
-//añade a cada div carta el evento
-for (carta of cartas) {
-    carta.addEventListener("click", mostrarImagenes);
-}
 
-
-function deseleccionar() {
-    if (comprobarIguales(cartasSeleccionadas)) {
-        cartasSeleccionadas.forEach(element => {
+function deseleccionar(cartas) {
+    if (comprobarIguales(cartas)) {
+        cartas.forEach(element => {
             element.removeEventListener("click", mostrarImagenes);
         });
 
     } else {
-        cartasSeleccionadas.forEach(element => {
+        cartas.forEach(element => {
             element.firstElementChild.removeChild(element.firstElementChild.lastElementChild);
             element.addEventListener("click", mostrarImagenes);
             //prodiamos crear una clase con dos estilos (inicial, acertada) y quitar o darla a los elementos
@@ -83,20 +90,123 @@ function deseleccionar() {
 
 }
 
+let barraInformativa = document.getElementById("barra_informativa");
 
 function comprobarIguales(arraySeleccionados) {
     if (arraySeleccionados[0].getAttribute("name") == arraySeleccionados[1].getAttribute("name")) {
         arraySeleccionados.forEach(element => {
             element.style.border = "3px solid green"
-        });
-        console.log("verdadero");
-        cont++;
-        return true;
-        /*function checkTotal(){
+            barraInformativa.innerHTML = "¡Has acertado!";
 
-        }*/
+        });
+
+        checkTotal()
+
+        return true;
     } else {
-        console.log("falso");
+        barraInformativa.innerHTML = "¡Has fallado!";
+        sumarErrores();
         return false
     }
 }
+
+function checkTotal() {
+    contParsed = parseInt(cont.textContent, 10)
+    contParsed++
+    cont.innerHTML = contParsed;
+    if (contParsed == 6) {
+        guardarPuntuacionFinal(this.usuario, errores.textContent);
+        alert(`Ganaste!! con ${errores.textContent} errores`)
+        window.location.reload();
+    }
+}
+
+function sumarErrores() {
+    errorParsed = parseInt(errores.textContent, 10)
+    errorParsed++
+    errores.innerHTML = errorParsed;
+}
+function guardarPuntuacionFinal(usuario, errores) {
+    let aux = localStorage.getItem("errores")
+
+
+    if (aux == null) {
+        aux = localStorage.setItem("errores", 100)
+    }
+
+    if (errores < aux) {
+        localStorage.setItem("usuario", usuario);
+    }
+    localStorage.setItem("errores", Math.min(localStorage.getItem("errores"), errores));
+}
+
+
+/* COOKIES */
+function setCookie(nombreCookie, valorCookie, expiraDia = 30) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expiraDia * 24 * 60 * 60 * 1000))
+    let expiracion = `expira=${date.toUTCString()}`;
+    document.cookie = `${nombreCookie}=${valorCookie}; ${expiracion};`;
+}
+
+function getCookie(nombreCookie) {
+    let nombre = nombreCookie + "=";
+    let arrayCookie = document.cookie.split(";");
+    for (let i = 0; i < arrayCookie.length; i++) {
+        let cookie = arrayCookie[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(nombre) == 0) {
+            return cookie.substring(nombre.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+//Idioma
+let btnsIdiomas = document.getElementsByClassName("idioma");
+Array.from(btnsIdiomas).forEach(element => {
+    element.addEventListener("click", cambiaIdioma);
+});
+
+function cambiaIdioma(evt) {
+    let idiomaClick = evt.target.id;
+    if (localStorage.getItem("idioma") == idiomaClick) {
+        return;
+    } else {
+        if (idiomaClick == "eng") {
+            localStorage.setItem("idioma", "eng");
+            cambiaTexto("eng");
+            evt.target.style.fontWeight = "bold";
+            document.getElementById("esp").style.fontWeight = "normal";
+        } else {
+            localStorage.setItem("idioma", "esp");
+            cambiaTexto("esp");
+            evt.target.style.fontWeight = "bold";
+            document.getElementById("eng").style.fontWeight = "normal";
+        }
+    }
+}
+function cambiaTexto(idioma) {
+    if (idioma == "eng") {
+        document.getElementById("estadisticas").innerHTML = "Stadistics";
+        document.getElementById("tagPuntuacion").innerHTML = "Score";
+        document.getElementById("tagErrores").innerHTML = "Errors";
+        document.getElementById("tagErroresTop").innerHTML = "Errors";
+        document.getElementById("leng").innerHTML = "Language";
+
+        document.getElementById("tagDesc").innerHTML = "Description";
+    } else {
+        document.getElementById("estadisticas").innerHTML = "Estadisticas";
+        document.getElementById("tagPuntuacion").innerHTML = "Puntuacion";
+        document.getElementById("tagErrores").innerHTML = "Errores";
+        document.getElementById("tagErroresTop").innerHTML = "Errores";
+        document.getElementById("leng").innerHTML = "Lenguaje";
+
+        document.getElementById("tagDesc").innerHTML = "Descripcion";
+    }
+
+}
+
+
