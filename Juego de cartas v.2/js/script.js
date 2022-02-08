e4fdebcfd86e1ce08f4e35430431e389a37580ec
sandrablano96 <sandrablano96@gmail.com>
2022-02-08 18:30:08 +0100

    cambios con bootstrap

--- Juego de cartas v.2/js/script.js
+++ Juego de cartas v.2/js/script.js
@@ -3,103 +3,160 @@ const cont = $("#contador");
 const errores = $("#errores");
 let cartasSeleccionadas = new Array();
 const cartas = $(".carta");
+const usuario = $("#nick").val();
 let imagen;
 let win, matchmsg, errormsg;
-
-const opciones = ["bart", "lisa", "bart", "maggie", "maggie", "homer", "flanders", "lisa", "flanders", "marge", "marge", "homer"];
-//crear funcion init que al cargar el doc carge el xml y el idioma segun la cookie
-//creamos el array de cartas
-const arrayOpciones = opciones.sort(function () { return Math.random() - 0.5 });
-
-window.onload = function init() {
-
-    if (localStorage.getItem("idioma") != null) {
-        cambiarIdiomaJSON(localStorage.getItem("idioma"));
-        
-    } else {
-        cambiarIdiomaJSON("esp");
-    }
-}
-
-for (let i = 0; i < arrayOpciones.length; i++) {
-    cartas[i].setAttribute("name", arrayOpciones[i]);
-}
-//DATOS USUARIO
-let usuario;
-do {
-    usuario = prompt("Introduce tu nombre de usuario",)
-} while (usuario === "" || usuario == null)
-    $("usuario").html(usuario);
+let audio = new Audio();
+const opciones = ["bart", "abuelo", "lisa", "bart", "maggie", "bomba", "maggie", "homer", "flanders", "lisa", "flanders", "marge", "marge", "homer", "abuelo"];
+const botonInicio = $("#botonInicio");
 
 //Top player
 const filaError = $("#jugadorTopFilaError");
 const jugadorTop = $("#jugadorTop");
 
-if (localStorage.getItem("errores") != null && localStorage.getItem("usuario") != null) {
-    $(filaError).html(localStorage.getItem("errores"))
-    $(jugadorTop).html(localStorage.getItem("usuario"))
-} else{
-    aux = localStorage.setItem("errores", 100);
+const barraInformativa = $("#barra_informativa");
+
+window.onload = function init() {
+
+    $(botonInicio).html('<i class="fas fa-play"></i>');
+    
+
+    if (localStorage.getItem("idioma") != null) {
+        cambiarIdiomaJSON(localStorage.getItem("idioma"));
+
+    } else {
+        cambiarIdiomaJSON("esp");
+    }
+    if (localStorage.getItem("errores") != null && localStorage.getItem("usuario") != null) {
+        $(filaError).html(localStorage.getItem("errores"))
+        $(jugadorTop).html(localStorage.getItem("usuario"))
+    } else {
+        aux = localStorage.setItem("errores", 100);
+    }
+    $(".carta").addClass("prevent-click");
+    
 }
 
-//añade a cada div carta el evento
-$(".carta").on("click",mostrarImagenes);
+$("#comenzar").click(function (){
+    if(usuario == ""){  
+        return;
+    }
+    $("#usuario").html(usuario);
+    $(errores).html(0);
+    barajar();
+    $(".modal").modal('hide');
+}); 
+
+function barajar() {
+    if ($(cartas).attr("name") != null) {
+        $(cartas).removeAttr("name");
+    }
+    const arrayOpciones = opciones.sort(function () { return Math.random() - 0.5 });
+    for (let i = 0; i < arrayOpciones.length; i++) {
+
+        cartas[i].setAttribute("name", arrayOpciones[i]);
+    }
+
+    $(".carta").on("click", mostrarImagenes);
+    $(".carta").removeClass("prevent-click");
+    $(cont).html(0);
+    
+}
+
 
 function mostrarImagenes(evt) {
     contadorClicks++;
     const carta = evt.target;
+    $(carta).off("click");
     if (carta != null) {
         const idCarta = $(carta).attr("name");
-        cartasSeleccionadas.push(carta);
         imagen = document.createElement("img");
         $(imagen).attr("src", "img/" + idCarta + ".png")
-        $(carta).children(".carta__imagen").append(imagen);
+        $(carta).children(".carta__imagen").html(imagen);
         $(imagen).attr("alt", idCarta);
         $(carta).addClass("cara-delantera");
-        $(carta).off(evt);
+        $("#audio").attr("src", "audio/click.mp3"); 0
+        $("#audio")[0].play();
+        cartasSeleccionadas.push(carta);
+        if (idCarta == "bomba") {
+            $("#audio").attr("src", "audio/bomba.mp3");
+            $("#audio")[0].play();
+            cartasSeleccionadas.splice(0, cartasSeleccionadas.length);
+            finDePartida();
+            return;
+        }
+
         if (contadorClicks == 2) {
+            $(botonInicio).html('<i class="fas fa-undo-alt"></i>');
             contadorClicks = 0;
             setTimeout(() => {
                 deseleccionar(cartasSeleccionadas);
             }, 500);
-
-
         }
     }
 }
 
+function finDePartida() {
+    contadorClicks = 0;
+    alert("¡BOOM! Vuelve a empezar");
+    $(cartas).off("click");
+    $(cartas).removeClass("cara-delantera");
+    $(cartas).removeClass("correcta");
+    $(cartas).each(function (index, element) {
+        $(element).children().first().empty();
+    })
+    $(botonInicio).html('<i class="fas fa-play"></i>');
+    $(barraInformativa).html(errormsg);
+    $(barraInformativa).removeClass("alert-success");
+    $(barraInformativa).addClass("alert-danger");
+    $('.progress-bar').css('width','0%').attr('aria-valuenow', '0');
+    barajar();
+
+}
+
 function deseleccionar(cartas) {
     if (comprobarIguales(cartas)) {
+        $("#audio").attr("src", "audio/correct.mp3");
+        $("#audio")[0].play();
         cartas.forEach(element => {
             $(element).off("click");
+            $(element).addClass("correcta");
+
         });
+        $(barraInformativa).removeClass("alert-danger");
+        $(barraInformativa).addClass("alert-success");
+        let progresoActual = parseInt($(".progress-bar").attr("aria-valuenow")) + 16.6;
+        $('.progress-bar').css('width', progresoActual + '%').attr('aria-valuenow', progresoActual);
+        $(barraInformativa).html(matchmsg);
+        $('.alert').alert();
+
+
 
     } else {
         cartas.forEach(element => {
+            $("#audio").attr("src", "audio/fallo.mp3");
+            $("#audio")[0].play();
             $(element).children().first().empty();
-            $(element).on("click",mostrarImagenes);
+            $(element).on("click", mostrarImagenes);
             $(element).removeClass("cara-delantera");
+            $(barraInformativa).removeClass("alert-success");
+            $(barraInformativa).addClass("alert-danger");
+            $(barraInformativa).html(errormsg);
+            $('.alert').alert();
+
         });
     };
 
     cartasSeleccionadas = [];
 }
 
-const barraInformativa = $("#barra_informativa");
 
 function comprobarIguales(arraySeleccionados) {
-    if ($(arraySeleccionados[0]).attr("name") == $(arraySeleccionados[1]).attr("name")) {
-        arraySeleccionados.forEach(element => {
-            $(element).addClass("correcta");
-            $(barraInformativa).html(matchmsg);
-        });
-
-        checkTotal()
-
+    if ($(arraySeleccionados[0]).attr("name") === $(arraySeleccionados[1]).attr("name")) {
+        checkTotal();
         return true;
     } else {
         sumarErrores();
-        $(barraInformativa).html(errormsg);
         return false;
     }
 }
@@ -110,8 +167,15 @@ function checkTotal() {
     $(cont).html(contParsed);
     if (contParsed == 6) {
         guardarPuntuacionFinal(this.usuario, errores.textContent);
-        alert(win)
-        window.location.reload();
+        $(barraInformativa).removeClass("alert-success");
+        $(barraInformativa).removeClass("alert-danger");
+        $(barraInformativa).addClass("alert-info");
+        $(barraInformativa).html(win);
+        $('.alert').alert();
+        setTimeout(() => {
+            window.location.reload();
+        }, 500);
+
     }
 }
 
@@ -127,12 +191,12 @@ function guardarPuntuacionFinal(usuario, errores) {
     if (aux != null && errores < aux) {
         localStorage.setItem("usuario", usuario);
         localStorage.setItem("errores", errores);
-        
-    } 
+        $(barraInformativa).html("¡Has ganado con un nuevo record!")
+    }
 }
 
 //Idioma
-$(".idioma").on("click",cambiaIdioma);
+$(".idioma").on("click", cambiaIdioma);
 
 function cambiaIdioma() {
     let idiomaClick = $(this).attr("id");
@@ -142,22 +206,22 @@ function cambiaIdioma() {
         if (idiomaClick == "eng") {
             localStorage.setItem("idioma", "eng");
             cambiarIdiomaJSON("eng");
-            $(this).css("fontWeight", "bold");
-            $("#esp").css("fontWeight", "normal" );
+            $(".btn").css("fontWeight", "bold");
+            $(".btn").css("fontWeight", "normal");
         } else {
             localStorage.setItem("idioma", "esp");
             cambiarIdiomaJSON("esp");
             $(this).css("fontWeight", "bold");
-            $("#eng").css("fontWeight", "normal" );
+            $("#eng").css("fontWeight", "normal");
         }
     }
 }
 
-function cambiarIdiomaJSON(idioma){
+function cambiarIdiomaJSON(idioma) {
     const xmlhttp = new XMLHttpRequest();
     const url = "lang.json";
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const myArr = JSON.parse(this.responseText);
             cambiaTexto(myArr, idioma);
@@ -168,33 +232,33 @@ function cambiarIdiomaJSON(idioma){
     xmlhttp.send();
 
     function cambiaTexto(arr, idioma) {
-            const lenguaje = arr["lang"][idioma];
+        const lenguaje = arr["lang"][idioma];
 
-            $("#estadisticas").html( lenguaje.STADISTICS);
+        $("#estadisticas").html(lenguaje.STADISTICS);
 
-            $("#barra_informativa").html(lenguaje.INFOBAR);
+        $("#barra_informativa").html(lenguaje.INFOBAR);
 
-            $("#tagPuntuacion").html(lenguaje.SCORE);
+        $("#tagPuntuacion").html(lenguaje.SCORE);
 
-            $("#tagErrores").html(lenguaje.ERRORS);
+        $("#tagErrores").html(lenguaje.ERRORS);
 
-            $("#tagErroresTop").html(lenguaje.ERRORS);
+        $("#tagErroresTop").html(lenguaje.ERRORS);
 
-            $("#tagTop").html(lenguaje.TOPPLAYER);
+        $("#tagTop").html(lenguaje.TOPPLAYER);
 
-            matchmsg = lenguaje.MATCHMESSAGE;
+        matchmsg = lenguaje.MATCHMESSAGE;
 
-            errormsg = lenguaje.ERRORMESSAGE;
+        errormsg = lenguaje.ERRORMESSAGE;
 
-            $("#tagDesc").html(lenguaje.DESC);
+        $("#tagDesc").html(lenguaje.DESC);
 
-            win = lenguaje.WIN;
+        win = lenguaje.WIN;
 
-            $("#tagDescCompleta").html(lenguaje.DESCRIPTION);
+        $("#tagDescCompleta").html(lenguaje.DESCRIPTION);
 
-            $("#leng").html(idioma);
+        $("#leng").html(idioma);
     }
-    
+
 }
 
 
